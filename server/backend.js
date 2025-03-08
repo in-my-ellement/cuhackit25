edc6040a23f7149d578d703a4d78aebc3bbd8e32
--global <--global>
2025-03-08 02:46:27 -0500

    working towards gesture recognition

--- server/backend.js
+++ server/backend.js
@@ -16,17 +16,37 @@ wss.on('connection', function connection(ws) {
   
     // IMPORTANT
     ws.on('message', function message(data) {
-        console.log(data); return;
-        // do some processing on the data
-        const arr = undefined;
+        // integrate?
+        const data = JSON.parse(data.toString())["data"]; 
+        const states = [];
 
-        // recognize the gesture
-        const gesture = undefined;
+        let v_x = 0; let v_y = 0; let v_z = 0;
+        let s_x = 0; let s_y = 0; let s_z = 0;
+        states.push(s_x, s_y, s_z);
+
+        for (let i = 0; i < data.length; i++) {
+            v_x += data[i]["x"]; v_y += data[i]["y"]; v_z += data[i]["z"];
+            s_x += v_x; s_y += v_y; s_z += v_z;
+            states.push(s_x, s_y, s_z);
+        }
+
+        // use the resulting positions
+        console.log(states);
+
+        // recognize a gesture with the points
+        let gesture = rec.Recognize(states, false);
+        console.log(gesture);
+
+        // data to send out to clients
+        let result = JSON.stringify({
+            user: "oops",
+            gesture: gesture
+        });
 
         // emit the gesture out to all connected clients
         wss.clients.forEach(function each (client) {
             if (client.readyState === WebSocket.OPEN) {
-                client.send(gesture, { binary: false });
+                client.send(result, { binary: false });
             }
         });
     });
